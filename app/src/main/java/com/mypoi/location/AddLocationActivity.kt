37e0ab939576280cc9026b73cb2602e409a816cb
HarounRossini <haroun.rossini@gmail.com>
2024-01-21 17:47:32 +0100

    defining Categories Spinner

--- app/src/main/java/com/mypoi/location/AddLocationActivity.kt
+++ app/src/main/java/com/mypoi/location/AddLocationActivity.kt
@@ -2,11 +2,26 @@ package com.mypoi.location
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.mypoi.R
+import database.Category
+import database.Database
 
 class AddLocationActivity : AppCompatActivity() {
+    var dbHelper = Database(this)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val writeDb = dbHelper.writableDatabase
+        val readDib = dbHelper.readableDatabase
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_location)
+        // get categories from db
+        val categories = dbHelper.getCategories(readDib)
+
+        val spinner = findViewById<Spinner>(R.id.categorySpinner)
+
+        val adapter = ArrayAdapter<Category>
+
+
     }
 }
--- app/src/main/java/database/Category.kt
+++ app/src/main/java/database/Category.kt
@@ -3,4 +3,7 @@ package database
 class Category {
      var id: Int = 0
      var title = String()
+     override fun toString(): String {
+          return title
+     }
 }
